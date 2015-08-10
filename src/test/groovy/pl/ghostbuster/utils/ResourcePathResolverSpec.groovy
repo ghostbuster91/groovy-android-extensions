77a19045efbd3b44ad82d4a4d2cda7c92a85ce73
ghost <kas_kon5@wp.pl>
2015-08-10 22:37:18 +0200

    a proof that implementation of ResourcePathResolver is wrong

--- src/test/groovy/pl/ghostbuster/utils/ResourcePathResolverSpec.groovy
+++ src/test/groovy/pl/ghostbuster/utils/ResourcePathResolverSpec.groovy
@@ -1,12 +1,27 @@
 package pl.ghostbuster.utils
 
+import groovy.transform.NotYetImplemented
 import spock.lang.Specification
 
 class ResourcePathResolverSpec extends Specification {
 
     def "Should find proper path to resource based on sourcePath and rClassPackageName"() {
         given:
-        String sourcePath = '/workspace/sampleApp/app/src/main/groovy/com/sample/app/domain/someHolder.groovy'
+        String sourcePath = 'file:/workspace/sampleApp/app/src/main/groovy/com/sample/app/domain/someHolder.groovy'
+        String rClassPackageName = 'com.sample.app'
+
+        when:
+        def resolver = new ResourcePathResolver(sourcePath, rClassPackageName)
+
+        then:
+        resolver.getPath('layout/my_row.xml') == '/workspace/sampleApp/app/src/main/res/layout/my_row.xml'
+    }
+
+    @NotYetImplemented
+//TODO: a proof that implementation is wrong
+    def "Should find proper path to resource when the transforming node package name is different then r class packege name"() {
+        given:
+        String sourcePath = 'file:/workspace/sampleApp/app/src/main/groovy/pl/waw/domain/someHolder.groovy'
         String rClassPackageName = 'com.sample.app'
 
         when:
